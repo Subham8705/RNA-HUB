37f78f0c2eb92cc4b1c1b0ff617344b7b47fcb64
Subham <subhamkumarshee@gmail.com>
2025-06-21 12:17:48 +0530

    added env

--- RNA/src/firebase.js
+++ RNA/src/firebase.js
@@ -21,13 +21,14 @@ import {
 
 // ✅ Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPgBF7-6eFre4poIR2CjVwp1mn32ItznQ",
-  authDomain: "rna-hub.firebaseapp.com",
-  projectId: "rna-hub",
-  storageBucket: "rna-hub.appspot.com",
-  messagingSenderId: "1051758926151",
-  appId: "1:1051758926151:web:8cc9e4380054acdb01d462",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // ✅ Initialize Firebase app
 const app = initializeApp(firebaseConfig);
